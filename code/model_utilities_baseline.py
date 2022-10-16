500553acb9f721f11112fc1be89ad4e8b9e171fe
klaypso <klaypso@proton.me>
2022-10-16 10:13:13 +0000

    13.1.0

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -5,4 +5,16 @@ from typing import Type, Any, Callable, Union, List, Optional
 import torch
 from torch import Tensor
 import torch.nn as nn
-fro
+from torch.hub import load_state_dict_from_url
+import torchvision
+
+
+
+# Create PyTorch Models
+# Model: DenseNet 121 (Baseline)
+class DenseNet121(torch.nn.Module):
+    def __init__(self, channels, height, width, nr_classes):
+        super(DenseNet121, self).__init__()
+
+        # Init variables
+        self.channels = channels
