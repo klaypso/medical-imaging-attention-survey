ef9364087e938f59bd149e5e49c2e23b8aea27ed
klaypso <klaypso@proton.me>
2022-10-20 14:29:33 +0000

    Version 1.2.6

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -59,4 +59,10 @@ class DenseNet121(torch.nn.Module):
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
     """3x3 convolution with padding"""
     
-    return
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+
+# Helper Function: ResNet Conv1x1 (https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py)
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 co
