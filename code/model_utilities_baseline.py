5a721e16450e65a41973adce31ecc18566923083
klaypso <klaypso@proton.me>
2022-10-25 17:24:28 +0000

    Updated README example

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -91,4 +91,14 @@ class BasicBlock(nn.Module):
         
         
         
-        # Both self.conv1 and self.downsample layers downsample the input when stride !
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu1 = nn.ReLU(inplace=False)
+        
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.relu2 = nn.ReLU(inplace=False)
+        
+        self.downsample = downsample
+        self.stride 
