5dd6711a2059b7e55a9fb5624986ff8de7baee87
klaypso <klaypso@proton.me>
2022-11-01 18:36:57 +0000

    Few Changes

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -134,4 +134,9 @@ class Bottleneck(nn.Module):
 
     expansion: int = 4
 
-    def __ini
+    def __init__(self, inplanes: int, planes: int, stride: int = 1, downsample: Optional[nn.Module] = None, groups: int = 1, base_width: int = 64, dilation: int = 1, norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
+        super().__init__()
+        
+        
+        if norm_layer is None:
+            no
