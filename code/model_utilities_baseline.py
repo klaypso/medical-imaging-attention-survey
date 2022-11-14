0d00180c27fb76cc899c3e022f783aeeba322ba7
klaypso <klaypso@proton.me>
2022-11-14 19:38:02 +0000

    SUNRPC: Add barriers to ensure read ordering in rpc_wake_up_task_queue_locked

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -225,4 +225,9 @@ class ResNet(nn.Module):
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
         self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        
