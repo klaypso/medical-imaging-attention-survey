c6c3c43922ba8c283632c25e515775f7227f9de0
klaypso <klaypso@proton.me>
2022-11-22 08:16:17 +0000

    Added header image

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -246,4 +246,11 @@ class ResNet(nn.Module):
         # so that the residual branch starts with zeros, and each residual block behaves like an identity.
         # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
         if zero_init_residual:
-     
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+
+    def _make_layer(
+        s
