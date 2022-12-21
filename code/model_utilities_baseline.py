d80e28a6acaf81c7d117c4ec1ee633e915c9fe54
klaypso <klaypso@proton.me>
2022-12-21 14:26:50 +0000

    Docker

--- code/model_utilities_baseline.py
+++ code/model_utilities_baseline.py
@@ -422,4 +422,17 @@ class VGG16(torch.nn.Module):
         # Compute in_features
         _in_features = torch.rand(1, self.channels, self.height, self.width)
         _in_features = self.vgg16(_in_features)
-        _in_features = _in_features.size(0) * _in_features.size(1)
+        _in_features = _in_features.size(0) * _in_features.size(1) * _in_features.size(2) * _in_features.size(3)
+
+        # Create FC1 Layer for classification
+        self.fc1 = torch.nn.Linear(in_features=_in_features, out_features=self.nr_classes)
+
+
+        return
+    
+
+    def forward(self, inputs):
+        # Compute Backbone features
+        features = self.vgg16(inputs)
+
+       
