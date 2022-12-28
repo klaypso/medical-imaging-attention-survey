8f2e636ddecd9e5b032b74d8140caeac18bab91b
klaypso <klaypso@proton.me>
2022-12-28 14:55:54 +0000

    Removes unused code

--- code/model_utilities_se.py
+++ code/model_utilities_se.py
@@ -17,4 +17,10 @@ def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, d
     
     """3x3 convolution with padding"""
 
-    return torch.nn.Co
+    return torch.nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+# Helper Function (from: https://pytorch.org/vision/stable/_modules/torchvision/models/resnet.html#resnet18)
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> torch.nn.Conv2d:
+    
+    ""
