210e6cd41f23ed0618b5dd47b833b8246c75dcdf
klaypso <klaypso@proton.me>
2022-12-28 18:08:18 +0000

    better tests

--- code/model_utilities_se.py
+++ code/model_utilities_se.py
@@ -23,4 +23,15 @@ def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, d
 # Helper Function (from: https://pytorch.org/vision/stable/_modules/torchvision/models/resnet.html#resnet18)
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> torch.nn.Conv2d:
     
-    ""
+    """1x1 convolution"""
+    
+    return torch.nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+
+# Squeeze-Excitation Layer (from: https://github.com/moskomule/senet.pytorch)
+class SELayer(torch.nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        
+    
