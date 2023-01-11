6dba5a0769e6c6436bc3e8cb8f80122433d616e2
klaypso <klaypso@proton.me>
2023-01-11 13:52:46 +0000

    Documentation updated.

--- code/model_utilities_se.py
+++ code/model_utilities_se.py
@@ -153,4 +153,9 @@ class SEResNet50(torch.nn.Module):
 
         # FC-Layers
         # Compute in_features
-        _in_features = torch.rand(1, self.channels, s
+        _in_features = torch.rand(1, self.channels, self.height, self.width)
+        _in_features = self.se_resnet50(_in_features)
+        _in_features = _in_features.size(0) * _in_features.size(1) * _in_features.size(2) * _in_features.size(3)
+
+        # Create FC1 Layer for classification
+        self.fc1 = torch.nn.Linear(in_features=_in_features, out_features=self.nr_class
