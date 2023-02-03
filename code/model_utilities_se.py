fd266c6fe7a178899aaa011a02fa4ebc548d76b0
klaypso <klaypso@proton.me>
2023-02-03 06:05:24 +0000

    Update changes file

--- code/model_utilities_se.py
+++ code/model_utilities_se.py
@@ -308,4 +308,12 @@ class _DenseLayer(torch.nn.Module):
         self.add_module('conv2', torch.nn.Conv2d(bn_size * growth_rate, growth_rate,
                                            kernel_size=3, stride=1, padding=1,
                                            bias=False))
-        self.drop_rate = f
+        self.drop_rate = float(drop_rate)
+        self.memory_efficient = memory_efficient
+
+    def bn_function(self, inputs: List[torch.Tensor]) -> torch.Tensor:
+        concated_features = torch.cat(inputs, 1)
+        bottleneck_output = self.conv1(self.relu1(self.norm1(concated_features)))  # noqa: T484
+        return bottleneck_output
+
+    # to
