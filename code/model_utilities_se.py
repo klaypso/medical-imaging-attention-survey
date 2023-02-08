f4fd40a1957401619377b0e9cdf52dbea3b400d2
klaypso <klaypso@proton.me>
2023-02-08 16:02:16 +0000

    [Version] Bumped.

--- code/model_utilities_se.py
+++ code/model_utilities_se.py
@@ -343,3 +343,13 @@ class _DenseLayer(torch.nn.Module):
     def forward(self, input: torch.Tensor) -> torch.Tensor:  # noqa: F811
         if isinstance(input, torch.Tensor):
             prev_features = [input]
+        else:
+            prev_features = input
+
+        if self.memory_efficient and self.any_requires_grad(prev_features):
+            if torch.jit.is_scripting():
+                raise Exception("Memory Efficient not supported in JIT")
+
+            bottleneck_output = self.call_checkpoint_bottleneck(prev_features)
+        else:
+   
