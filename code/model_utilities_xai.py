c424c6ec113e0c1f4979e82a37fac2f665b6c144
klaypso <klaypso@proton.me>
2023-03-01 15:29:45 +0000

    Fixed readme typo

--- code/model_utilities_xai.py
+++ code/model_utilities_xai.py
@@ -1,4 +1,17 @@
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
-from collec
+from collections import defaultdict
+import cv2
+
+# PyTorch Imports
+import torch
+import torch.nn as nn
+
+# Captum Imports
+from captum.attr import DeepLift, LRP
+from captum.attr._utils.custom_modules import Addition_Module
+from captum.attr._utils.lrp_rules import EpsilonRule, PropagationRule
+
+# Transformer xAI Imports
+from transfor
