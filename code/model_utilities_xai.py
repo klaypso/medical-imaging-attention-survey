fa2c8dfd036e027da258a0ccf8e462a8498563d1
klaypso <klaypso@proton.me>
2023-03-03 06:24:44 +0000

    Speling

--- code/model_utilities_xai.py
+++ code/model_utilities_xai.py
@@ -14,4 +14,14 @@ from captum.attr._utils.custom_modules import Addition_Module
 from captum.attr._utils.lrp_rules import EpsilonRule, PropagationRule
 
 # Transformer xAI Imports
-from transfor
+from transformer_explainability_utils.ViT_explanation_generator import LRP as DeiT_LRP
+from transformer_explainability_utils.ViT_explanation_generator import Baselines as DeiT_Baselines
+
+# Project Imports
+from model_utilities_cbam import ChannelPool
+
+
+# Fix Random Seeds
+random_seed = 42
+torch.manual_seed(random_seed)
+np.random.seed(rand
