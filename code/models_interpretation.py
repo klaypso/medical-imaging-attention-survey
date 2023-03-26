f45bca380551ecbf8cc08b7f676ddeee64ffcf12
klaypso <klaypso@proton.me>
2023-03-26 13:09:24 +0000

    drivers/rtc/rtc-spear.c: fix use-after-free in spear_rtc_remove()

--- code/models_interpretation.py
+++ code/models_interpretation.py
@@ -13,4 +13,13 @@ import torchvision
 from data_utilities import APTOSDataset, ISIC2020Dataset, MIMICXRDataset
 from model_utilities_baseline import DenseNet121, ResNet50
 from model_utilities_cbam import CBAMDenseNet121, CBAMResNet50
-from model_utilities_xai import 
+from model_utilities_xai import generate_post_hoc_xmap, gen_transformer_att
+from model_utilities_se import SEDenseNet121, SEResNet50
+from transformers import DeiTFeatureExtractor
+from transformer_explainability_utils.ViT_LRP import deit_tiny_patch16_224 as DeiT_Tiny 
+
+
+
+# Fix Random Seeds
+random_seed = 42
+torch.manual_seed(random_seed)
