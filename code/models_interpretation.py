9ad0703db502622f11911285402f7bd16b5065f4
klaypso <klaypso@proton.me>
2023-03-29 21:05:46 +0000

    Update njrat.txt

--- code/models_interpretation.py
+++ code/models_interpretation.py
@@ -54,4 +54,13 @@ parser.add_argument('--batchsize', type=int, default=4, help="Batch-size for tra
 parser.add_argument('--imgsize', type=int, default=224, help="Size of the image after transforms")
 
 # Resize
-parser.add_argument('--resize', type=str, choices=["direct_resize", "resizeshortest_randomcrop"], default="
+parser.add_argument('--resize', type=str, choices=["direct_resize", "resizeshortest_randomcrop"], default="direct_resize", help="Resize data transformation")
+
+# Number of workers
+parser.add_argument("--num_workers", type=int, default=0, help="Number of workers for dataloader")
+
+# GPU ID
+parser.add_argument("--gpu_id", type=int, default=0, help="The index of the GPU")
+
+# Number of layers (ViT)
+parser.add_argument("--nr_layers", type=int
