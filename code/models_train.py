f089e9341e6106781f6587a62e1d40cbe0460420
klaypso <klaypso@proton.me>
2023-04-22 09:51:03 +0000

    updated matchresponses.csv

--- code/models_train.py
+++ code/models_train.py
@@ -29,4 +29,16 @@ from model_utilities_se import SEDenseNet121, SEResNet50
 from model_utilities_cbam import CBAMDenseNet121, CBAMResNet50
 from data_utilities import APTOSDataset, ISIC2020Dataset, MIMICXRDataset
 from transformers import DeiTFeatureExtractor
-from transformer_explainability_utils.ViT_LRP import deit
+from transformer_explainability_utils.ViT_LRP import deit_tiny_patch16_224 as DeiT_Tiny
+
+
+# Command Line Interface
+# Create the parser
+parser = argparse.ArgumentParser()
+
+# Add the arguments
+# Data directory
+parser.add_argument('--data_dir', type=str, required=True, help="Directory of the data set.")
+
+# Data set
+parser.add_argument('--dataset', type=str, required=True, choices=["APTOS2019", "ISI
