7fe8355b4ae2feb7cbd56ccb139ecaa8fed32612
klaypso <klaypso@proton.me>
2023-04-29 21:34:36 +0000

    minor corrections

--- code/models_train.py
+++ code/models_train.py
@@ -178,4 +178,10 @@ train_transforms = torchvision.transforms.Compose([
     torchvision.transforms.RandomAffine(degrees=(-10, 10), translate=(0.05, 0.1), scale=(0.95, 1.05), shear=0, resample=0, fillcolor=(0, 0, 0)),
     torchvision.transforms.RandomHorizontalFlip(p=0.5),
     torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize(mean=feature_extractor.i
+    torchvision.transforms.Normalize(mean=feature_extractor.image_mean if feature_extractor else MEAN, std=feature_extractor.image_std if feature_extractor else STD)
+])
+
+
+# Validation Transforms
+val_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(IMG_SIZE if resize_opt == 'resizeshortest_randomcrop' else (IMG_SIZE, IMG_SIZE)
