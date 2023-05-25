e5a522b30da8846221621bb06f9f1a850f8d2920
klaypso <klaypso@proton.me>
2023-05-25 12:24:10 +0000

    msm: cpufreq: Update frequency index

--- code/plot_graphs_low_data_reg.py
+++ code/plot_graphs_low_data_reg.py
@@ -8,4 +8,13 @@ import matplotlib.pyplot as plt
 xl = pd.ExcelFile("results/experiments.xlsx")
 
 # Get datasets' names
-d
+datasets = xl.sheet_names
+
+# Parameters for the plot
+x = [1, 2, 3, 4]
+x_labels = ["1%", "10%", "50%", "100%"]
+colors = ['C0', 'C1', 'C0', 'C1', 'C0', 'C1', 'C2']
+style = ['solid', 'solid', 'dashed', 'dashed', 'dotted', 'dotted', 'solid']
+markers = ['s', 'D', 's', 'D', 's', 'D', 'o']
+
+# Got through the
