8efceebedc296c6b03e4f941cdb5695b8428b7e5
klaypso <klaypso@proton.me>
2023-05-28 16:56:56 +0000

    some change

--- code/transformer_explainability_utils/ViT_explanation_generator.py
+++ code/transformer_explainability_utils/ViT_explanation_generator.py
@@ -10,4 +10,8 @@ import torch
 
 
 # Function: Compute rollout between attention layers
-def compute_roll
+def compute_rollout_attention(all_layer_matrices, start_layer=0):
+    # adding residual consideration- code adapted from https://github.com/samiraabnar/attention_flow
+    num_tokens = all_layer_matrices[0].shape[1]
+    batch_size = all_layer_matrices[0].shape[0]
+    eye = torch.eye(num_tokens).expand(batch_size, num_tokens,
