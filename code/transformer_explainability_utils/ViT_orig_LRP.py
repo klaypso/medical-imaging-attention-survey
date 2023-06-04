73153446bef22ae7af16cc7d2418b5543b72fa2d
klaypso <klaypso@proton.me>
2023-06-04 18:03:34 +0000

    hoho

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -22,4 +22,16 @@ def _cfg(url='', **kwargs):
         'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
         'crop_pct': .9, 'interpolation': 'bicubic',
         'first_conv': 'patch_embed.proj', 'classifier': 'head',
-       
+        **kwargs
+    }
+
+
+
+# Dictionary: Default configurations
+default_cfgs = {
+    # patch models
+    'vit_small_patch16_224': _cfg(
+        url='https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-weights/vit_small_p16_224-15ec54c9.pth',
+    ),
+    'vit_base_patch16_224': _cfg(
+        url='https://github.com/rwig
