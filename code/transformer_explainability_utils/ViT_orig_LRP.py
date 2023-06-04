54bfaa5e2a0afe3ddfdcde6f106efef01491cf1f
klaypso <klaypso@proton.me>
2023-06-04 08:51:27 +0000

    。

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -1,1 +1,14 @@
-# Source: https://github.com/hila-chefer/Transforme
+# Source: https://github.com/hila-chefer/Transformer-Explainability
+# Vision Transformer (ViT) in PyTorch
+# Hacked together by / Copyright 2020 Ross Wightman
+
+# PyTorch Imports
+import torch
+import torch.nn as nn
+from einops import rearrange
+
+# Project Imports
+from layers_lrp import *
+from helpers import load_pretrained
+from weight_init import trunc_normal_
+from layer_helpers imp
