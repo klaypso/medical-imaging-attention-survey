a8b276da58ccb5b4bfe51bdd28b473728a007168
klaypso <klaypso@proton.me>
2023-06-04 14:45:10 +0000

    USB: kaweth.c: use GFP_ATOMIC under spin_lock

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -11,4 +11,15 @@ from einops import rearrange
 from layers_lrp import *
 from helpers import load_pretrained
 from weight_init import trunc_normal_
-from layer_helpers imp
+from layer_helpers import to_2tuple
+
+
+
+# Function: Get configuration
+def _cfg(url='', **kwargs):
+    return {
+        'url': url,
+        'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
+        'crop_pct': .9, 'interpolation': 'bicubic',
+        'first_conv': 'patch_embed.proj', 'classifier': 'head',
+       
