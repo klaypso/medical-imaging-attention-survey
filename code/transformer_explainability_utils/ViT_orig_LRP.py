bfea3b8f25739a71132772b473eefb3cad9b1023
klaypso <klaypso@proton.me>
2023-06-05 16:59:37 +0000

    drm/qxl: Do not cause spice-server to clean our objects

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -52,4 +52,14 @@ def compute_rollout_attention(all_layer_matrices, start_layer=0):
     eye = torch.eye(num_tokens).expand(batch_size, num_tokens, num_tokens).to(all_layer_matrices[0].device)
     all_layer_matrices = [all_layer_matrices[i] + eye for i in range(len(all_layer_matrices))]
     # all_layer_matrices = [all_layer_matrices[i] / all_layer_matrices[i].sum(dim=-1, keepdim=True)
-    #                       for i i
+    #                       for i in range(len(all_layer_matrices))]
+    joint_attention = all_layer_matrices[start_layer]
+    for i in range(start_layer+1, len(all_layer_matrices)):
+        joint_attention = all_layer_matrices[i].bmm(joint_attention)
+    return joint_attention
+
+
+
+# Class: Mlp
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_featu
