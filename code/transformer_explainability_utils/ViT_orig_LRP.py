04f8d4fed8044d25b750dc420f722016b257064c
klaypso <klaypso@proton.me>
2023-06-12 10:44:10 +0000

    Remove obsolete function

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -201,4 +201,14 @@ class Block(nn.Module):
         self.norm1 = LayerNorm(dim, eps=1e-6)
         self.attn = Attention(
             dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-        self.n
+        self.norm2 = LayerNorm(dim, eps=1e-6)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
+
+        self.add1 = Add()
+        self.add2 = Add()
+        self.clone1 = Clone()
+        self.clone2 = Clone()
+
+    def forward(self, x):
+        x1, x2 = self.cl
