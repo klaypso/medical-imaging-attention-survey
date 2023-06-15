df795de76a4eadc337f0b9c7b0a9d881083c5fc5
klaypso <klaypso@proton.me>
2023-06-15 12:37:20 +0000

    house keeping

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -231,4 +231,12 @@ class Block(nn.Module):
 
 
 
-# Cla
+# Class: PatchEmbed
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] //
