c2f82ac636056bc1691345438a2a209b0a9639d3
klaypso <klaypso@proton.me>
2023-06-28 06:00:02 +0000

    Added sample config file

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -396,4 +396,15 @@ class VisionTransformer(nn.Module):
                 grad = grad[0].reshape(-1, grad.shape[-1], grad.shape[-1])
                 cam = grad * cam
             cam = cam.clamp(min=0).mean(dim=0)
-            cam = cam[0, 1
+            cam = cam[0, 1:]
+            return cam
+
+        elif method == "last_layer_attn":
+            cam = self.blocks[-1].attn.get_attn()
+            cam = cam[0].reshape(-1, cam.shape[-1], cam.shape[-1])
+            cam = cam.clamp(min=0).mean(dim=0)
+            cam = cam[0, 1:]
+            return cam
+
+        elif method == "second_layer":
+       
