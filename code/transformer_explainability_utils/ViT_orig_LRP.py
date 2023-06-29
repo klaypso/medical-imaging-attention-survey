d8e5e6d90eaded6fe03eac3977fb8910c9bc1bb3
klaypso <klaypso@proton.me>
2023-06-29 12:04:05 +0000

    Add missing quote

--- code/transformer_explainability_utils/ViT_orig_LRP.py
+++ code/transformer_explainability_utils/ViT_orig_LRP.py
@@ -424,4 +424,14 @@ def _conv_filter(state_dict, patch_size=16):
     """ convert patch embedding weight from manual patchify + linear proj to conv"""
     out_dict = {}
     for k, v in state_dict.items():
-        if 'p
+        if 'patch_embed.proj.weight' in k:
+            v = v.reshape((v.shape[0], 3, patch_size, patch_size))
+        out_dict[k] = v
+    return out_dict
+
+
+
+# Function: Build vit_base_patch16_224
+def vit_base_patch16_224(pretrained=False, **kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_rati
