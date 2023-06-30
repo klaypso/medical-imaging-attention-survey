460475622475827a22f93fa627e6589b875a80b9
klaypso <klaypso@proton.me>
2023-06-30 21:46:36 +0000

    :arrow_up:

--- code/transformer_explainability_utils/helpers.py
+++ code/transformer_explainability_utils/helpers.py
@@ -24,4 +24,11 @@ def load_state_dict(checkpoint_path, use_ema=False):
     if checkpoint_path and os.path.isfile(checkpoint_path):
         checkpoint = torch.load(checkpoint_path, map_location='cpu')
         state_dict_key = 'state_dict'
-        if isinstance(checkpoint, dict
+        if isinstance(checkpoint, dict):
+            if use_ema and 'state_dict_ema' in checkpoint:
+                state_dict_key = 'state_dict_ema'
+        if state_dict_key and state_dict_key in checkpoint:
+            new_state_dict = OrderedDict()
+            for k, v in checkpoint[state_dict_key].items():
+                # strip `module.` prefix
+        
