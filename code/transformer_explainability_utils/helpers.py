280af4b9a85a882e6d337d77c50a94428183f0a5
klaypso <klaypso@proton.me>
2023-07-18 08:21:24 +0000

    cpufreq: Fix policy getting stuck when user & kernel min/max don't overlap

--- code/transformer_explainability_utils/helpers.py
+++ code/transformer_explainability_utils/helpers.py
@@ -255,3 +255,9 @@ def adapt_model_from_string(parent_module, model_string):
         if isinstance(old_module, nn.Linear):
             # FIXME extra checks to ensure this is actually the FC classifier layer and not a diff Linear layer?
             num_features = state_dict[n + '.weight'][1]
+            new_fc = nn.Linear(
+                in_features=num_features, out_features=old_module.out_features, bias=old_module.bias is not None)
+            set_layer(new_module, n, new_fc)
+            if hasattr(new_module, 'num_features'):
+                new_module.num_features = num_features
+    new_modul
