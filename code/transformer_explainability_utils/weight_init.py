dc181e2ae7dd6c4ea9b6d771af1d83da11e5a65a
klaypso <klaypso@proton.me>
2023-09-28 10:11:17 +0000

    v 1.0.1

--- code/transformer_explainability_utils/weight_init.py
+++ code/transformer_explainability_utils/weight_init.py
@@ -1,4 +1,17 @@
 # Source: https://github.com/hila-chefer/Transformer-Explainability
 
 # Imports
-imp
+import math
+import warnings
+
+# PyTorch Imports
+import torch
+
+
+
+# Function
+def _no_grad_trunc_normal_(tensor, mean, std, a, b):
+    # Cut & paste from PyTorch official master until it's in a few official releases - RW
+    # Method based on https://people.sc.fsu.edu/~jburkardt/presentations/truncated_normal.pdf
+    def norm_cdf(x):
+        # Computes standard normal cumul
