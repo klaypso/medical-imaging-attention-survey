5d8cb4b0502084ac1ecc30137dff929e7bdfdceb
klaypso <klaypso@proton.me>
2023-10-01 12:09:59 +0000

    Add R benchmark

--- code/transformer_explainability_utils/weight_init.py
+++ code/transformer_explainability_utils/weight_init.py
@@ -14,4 +14,12 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
     # Cut & paste from PyTorch official master until it's in a few official releases - RW
     # Method based on https://people.sc.fsu.edu/~jburkardt/presentations/truncated_normal.pdf
     def norm_cdf(x):
-        # Computes standard normal cumul
+        # Computes standard normal cumulative distribution function
+        return (1. + math.erf(x / math.sqrt(2.))) / 2.
+
+    if (mean < a - 2 * std) or (mean > b + 2 * std):
+        warnings.warn("mean is more than 2 std from [a, b] in nn.init.trunc_normal_. "
+                      "The distribution of values may be incorrect.",
+                      stacklevel=2)
+
+ 
