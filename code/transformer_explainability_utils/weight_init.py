7cc22e44cbe17f9fe42409dbc3f985eadbf50315
klaypso <klaypso@proton.me>
2023-10-12 22:16:03 +0000

    Removed unnecessary imports.

--- code/transformer_explainability_utils/weight_init.py
+++ code/transformer_explainability_utils/weight_init.py
@@ -50,4 +50,9 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
 
 # Function: Fills the input Tensor with values drawn from a truncated normal distribution
 def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
-    # type: (Tensor, float, float, float,
+    # type: (Tensor, float, float, float, float) -> Tensor
+    r"""Fills the input Tensor with values drawn from a truncated
+    normal distribution. The values are effectively drawn from the
+    normal distribution :math:`\mathcal{N}(\text{mean}, \text{std}^2)`
+    with values outside :math:`[a, b]` redrawn until they are within
+    the bounds. The
