b1156e1fc74b3a4fc3ce1d4f2917350c531112cc
klaypso <klaypso@proton.me>
2023-10-30 18:59:33 +0000

    _automated cleaning core module version comment spaces

--- code/transformer_explainability_utils/weight_init.py
+++ code/transformer_explainability_utils/weight_init.py
@@ -55,4 +55,12 @@ def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
     normal distribution. The values are effectively drawn from the
     normal distribution :math:`\mathcal{N}(\text{mean}, \text{std}^2)`
     with values outside :math:`[a, b]` redrawn until they are within
-    the bounds. The
+    the bounds. The method used for generating the random values works
+    best when :math:`a \leq \text{mean} \leq b`.
+    Args:
+        tensor: an n-dimensional `torch.Tensor`
+        mean: the mean of the normal distribution
+        std: the standard deviation of the normal distribution
+        a: the minimum cutoff value
+        b: the maximum cutoff value
+    Exam
